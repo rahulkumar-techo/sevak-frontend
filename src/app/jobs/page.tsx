bddcf14f12c29082584c4c07120984a7b54bac2d
Rahul kumar <rahulkumar7250883289@gmail.com>
2025-10-10 04:43:58 +0530

    added job files

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/Header'
 import React from 'react'
+import ListsOfJobs from './ListsOfJobs'
 
 type Props = {}
 
@@ -7,9 +8,11 @@ const page = (props: Props) => {
   return (
     <div>
         <Header
-        
         activeItem={1}
       />
+     <div className='mt-[60px]'>
+       <ListsOfJobs/>
+     </div>
     </div>
   )
 }
--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -12,7 +12,9 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
-export function ModeToggle() {
+export function ModeToggle(
+    {theme}:{theme:string}
+) {
     const { setTheme } = useTheme()
 
     return (
